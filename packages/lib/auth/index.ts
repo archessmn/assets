012c0294546cb90ae061e3009d87459173599149
Mia Moir <me@archess.mn>
2026-04-05 23:56:36 +0100

    fix: Import db correctly in lib auth

--- packages/lib/auth/index.ts
+++ packages/lib/auth/index.ts
@@ -1,8 +1,8 @@
-import { db } from "@repo/lib/db";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { genericOAuth } from "better-auth/plugins";
 import { env } from "../env";
+import { db } from "../db";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, { provider: "postgresql" }),
