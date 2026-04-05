e950f4c7e07d01bd1e7dcb8470a802534be3c1e9
Mia Moir <me@archess.mn>
2026-04-05 22:29:37 +0100

    chore: CI Take 4
    Those vars don't exist currently

--- packages/lib/minio/index.ts
+++ packages/lib/minio/index.ts
@@ -1,27 +1,27 @@
-import * as Minio from "minio";
+// import * as Minio from "minio";
 
-import { env } from "../env";
+// import { env } from "../env";
 
-export const getMinioClient = () => {
-  return new Minio.Client({
-    endPoint: env.MINIO_ENDPOINT,
-    useSSL: env.MINIO_USE_SSL === "true",
-    accessKey: env.MINIO_ACCESS_KEY,
-    secretKey: env.MINIO_SECRET_KEY,
-    region: env.MINIO_REGION,
-  });
-};
+// export const getMinioClient = () => {
+//   return new Minio.Client({
+//     endPoint: env.MINIO_ENDPOINT,
+//     useSSL: env.MINIO_USE_SSL === "true",
+//     accessKey: env.MINIO_ACCESS_KEY,
+//     secretKey: env.MINIO_SECRET_KEY,
+//     region: env.MINIO_REGION,
+//   });
+// };
 
-export async function checkMinioConnection() {
-  console.log("┌ Checking Minio connection");
+// export async function checkMinioConnection() {
+//   console.log("┌ Checking Minio connection");
 
-  const exists = await getMinioClient().bucketExists(env.MINIO_BUCKET);
+//   const exists = await getMinioClient().bucketExists(env.MINIO_BUCKET);
 
-  if (!exists) {
-    console.error(`└ 
-        Failed to connect to bucket ${env.MINIO_BUCKET} at ${
-          env.MINIO_ENDPOINT
-        } ${env.MINIO_USE_SSL === "true" ? "using SSL" : "without SSL"}`);
-  }
-  console.log("└ Connection to Minio sucessful");
-}
+//   if (!exists) {
+//     console.error(`└
+//         Failed to connect to bucket ${env.MINIO_BUCKET} at ${
+//           env.MINIO_ENDPOINT
+//         } ${env.MINIO_USE_SSL === "true" ? "using SSL" : "without SSL"}`);
+//   }
+//   console.log("└ Connection to Minio sucessful");
+// }
